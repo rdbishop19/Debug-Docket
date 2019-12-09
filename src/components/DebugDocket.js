c462b07388e60e4eb1f807d09dae1d08d0601909
Ryan Bishop <rdbishop06@gmail.com>
2019-12-09 15:16:30 -0600

    database dummy data

---
+++ src/components/DebugDocket.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class DebugDocket extends Component {
+	render() {
+		return (
+			<React.Fragment>
+                <h3>NAVBAR - DEBUG DOCKET</h3>
+				<div>MAIN APP VIEW</div>
+			</React.Fragment>
+		);
+	}
+}
+
+export default DebugDocket;
