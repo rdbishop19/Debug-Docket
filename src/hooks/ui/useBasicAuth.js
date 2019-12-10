7ce59f75c339476c10ae18e814403443a2f61a44
Ryan Bishop <rdbishop06@gmail.com>
2019-12-10 09:22:22 -0600

    auth folders/files moved to correct place

---
+++ src/hooks/ui/useBasicAuth.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const useBasicAuth = () => {
+
+    // hooks useState setup
+    const [loggedIn, setIsLoggedIn] = useState(false)
+
+    // similar to function we used in functional components previously
+    const isAuthenticated = () =>
+        loggedIn
+        || localStorage.getItem("credentials") !== null
+        || sessionStorage.getItem("credentials") !== null
+
+    // accept validated user inputs and set local/session Storage based
+    // on user 'remember be' settings
+    const login = (email, password, storageType = localStorage) => {
+        storageType.setItem(
+            "credentials",
+            JSON.stringify({
+                email: email,
+                password: password
+            })
+        )
+        // change state to logged in
+        setIsLoggedIn(true)
+    }
+
+    // log out user by changing state and removing local/session storage
+    const logout = () => {
+        setIsLoggedIn(false)
+        localStorage.removeItem("credentials")
+        sessionStorage.removeItem("credentials")
+    }
+
+    // return an object with these functions so they can be used elsewhere
+    return { isAuthenticated, logout, login}
+        
+}
+
+export default useBasicAuth
