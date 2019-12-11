8dfaf94c388dadf167f5bd58b9f6274f5008c942
Ryan Bishop <rdbishop06@gmail.com>
2019-12-11 10:57:05 -0600

    setting up edit form

--- src/components/entries/EntriesContainer.js
+++ src/components/entries/EntriesContainer.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import EntriesHomeContainer from './EntriesHomeContainer'
 import { Route } from 'react-router-dom'
 import EntryEdit from './EntryEdit'
-export class EntriesContainer extends Component {
-    render() {
-        const { match } = this.props
-        return (
-            <div style={{ flex: 1, minWidth: "375px"}}>
 
-                <Route exact path={`${match.path}`} render={(props)=>{
-                    return <EntriesHomeContainer {...props} />
-                }} />
-                {/* Note: had to use doubleslash for regex after changing path to JS expression */}
-                <Route path={`${match.path}/:entryId(\\d+)/edit`} render={(props)=>{
-                    return <EntryEdit {...props} />
-                }} />
+export default function EntriesContainer(props) {
+    const { match } = props
+    
+    return (
+        <div style={{ flex: 1, minWidth: "375px"}}>
 
-            </div>
-        )
-    }
+            <Route exact path={`${match.path}`} render={(props)=>{
+                return <EntriesHomeContainer {...props} />
+            }} />
+            {/* Note: had to use doubleslash for regex after changing path to JS expression */}
+            <Route path={`${match.path}/:entryId(\\d+)/edit`} render={(props)=>{
+                return <EntryEdit {...props} />
+            }} />
+
+        </div>
+    )
 }
-
-export default EntriesContainer
--- src/components/entries/EntryEdit.js
+++ src/components/entries/EntryEdit.js
@@ -1,13 +1,116 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Paper, Typography, FormControl, InputLabel, Input, RadioGroup, Radio, FormControlLabel, FormLabel } from '@material-ui/core'
+import Settings from '../../repositories/Settings'
 
-export class EntryEdit extends Component {
-    render() {
-        return (
-            <div>
-                ENTRY EDIT FORM
-            </div>
-        )
+
+export default function EntryEdit() {
+
+    const initialEntry = {
+        title: '',
+        description: '',
+        id: '',
+        isCompleted: false,
+        priority: {
+            id: '',
+            label: ''
+        },
+        severity: {
+            id: '',
+            label: ''
+        },
+        category: {
+            id: '',
+            label: ''
+        },
     }
+
+    const [entry, setEntry] = useState(initialEntry)
+    // const [title, setTitle] = useState('')
+    const [priorities, setPriorities] = useState([])
+    const [severities, setSeverities] = useState([])
+    const [categories, setCategories] = useState([])
+
+    useEffect(()=>{
+        fetch(`${Settings.remoteURL}/entries/1?_expand=severity&_expand=priority&_expand=category`)
+        .then((data)=>data.json())
+        .then((data)=> {
+            console.log('fetch', data)
+            setEntry(data)
+            // setTitle(data.title)
+        })
+        //TODO: move to separate component
+        fetch(`${Settings.remoteURL}/priorities`)
+        .then((data)=>data.json())
+        .then((data)=>{
+            setPriorities(data)
+        })
+        //TODO: move to separate component
+        fetch(`${Settings.remoteURL}/severities`)
+        .then((data)=>data.json())
+        .then((data)=>{
+            setSeverities(data)
+        })
+        //TODO: move to separate component
+        fetch(`${Settings.remoteURL}/categories`)
+        .then((data)=>data.json())
+        .then((data)=>{
+            setCategories(data)
+        })
+    }, [])
+
+    const { title, description, priority, severity, category } = entry
+    console.log('priority', priority)
+
+    return (
+        <Paper>
+            <form style={{ width: "50%", textAlign: "center", margin: "0 auto"}}>
+                <Typography>Bug Ticket</Typography>
+                <FormControl component="fieldset">
+                    <InputLabel>Title:</InputLabel>
+                    <Input value={title}></Input>
+                </FormControl>
+                <FormControl>
+                    <InputLabel>Description:</InputLabel>
+                    <Input value={description}></Input>
+                </FormControl>
+                <FormControl>
+                    <FormLabel>Priority:</FormLabel>
+                    <RadioGroup value={priority.id} aria-label="priority">
+                        {priorities.map((priority)=>{
+                            return <FormControlLabel value={priority.id} label={priority.label} control={
+                                <Radio color="default" />} />
+                        })}
+                    </RadioGroup>
+                </FormControl>
+                <FormControl>
+                    <FormLabel>Severity:</FormLabel>
+                    <RadioGroup value={severity.id} aria-label="severity">
+                        {severities.map((severity)=>{
+                            return <FormControlLabel value={severity.id} label={severity.label} control={
+                                <Radio color="default" />} />
+                        })}
+                    </RadioGroup>
+                </FormControl>
+                <FormControl>
+                    <FormLabel>Category:</FormLabel>
+                    <RadioGroup value={category.id} aria-label="category">
+                        {categories.map((category)=>{
+                            return <FormControlLabel value={category.id} label={category.label} control={
+                                <Radio color="default" />} />
+                        })}
+                    </RadioGroup>
+                </FormControl>
+                <FormControl>
+                    <FormLabel>Status:</FormLabel>
+                    <RadioGroup defaultValue="0" aria-label="status">
+                    <FormControlLabel value="0" control={
+                        <Radio color="default" />} label="Open" />
+                        <FormControlLabel value="1" control={
+                        <Radio color="default" />} label="Closed"/>
+                    </RadioGroup>
+                </FormControl>
+            </form>
+        </Paper>
+    )
 }
 
-export default EntryEdit
