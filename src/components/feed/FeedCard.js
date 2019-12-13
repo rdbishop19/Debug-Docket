e6fed83efd74bdf18ed24bc6e1b626b4df52ceae
Ryan Bishop <rdbishop06@gmail.com>
2019-12-13 15:23:12 -0600

    history view for user entries displaying correctly

--- src/components/feed/FeedCard.js
+++ src/components/feed/FeedCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Card, CardContent } from '@material-ui/core';
 
 /* 
     Purpose: display the contents of each bug entry with styling and functionality
@@ -9,14 +10,15 @@ function convertDateTimeFromISO(date) {
     return new Date(date)
 }
 
-export default function FeedCard({ entry, activeUser }) {
+export default function FeedCard({ entry, activeUser, container }) {
     const { user } = entry
     // console.log('entry', entry)
-    
+    const feed = container === "feed"
+    const history = container === "history"
     //TODO: update this later during styline time
 	let entryStyle = {
         backgroundColor: 'cornsilk',
-        margin: "0 auto",
+        margin: "10px 10px",
         padding: "5px"
 	};
 	if (entry.userId === activeUser.id) {
@@ -26,13 +28,15 @@ export default function FeedCard({ entry, activeUser }) {
 		};
 	}
 	return (
-		<div key={entry.id} style={entryStyle}>
-			<span>{user.firstName} {user.lastName} </span>
-            {activeUser.id === entry.userId && <span>(you) </span>}
-            <span>on </span><span>{convertDateTimeFromISO(entry.timeStarted).toLocaleString()}</span>
-            <p>{entry.title}</p>
-
-
-		</div>
+		<Card key={entry.id} style={entryStyle}>
+            <CardContent>
+                {feed && <span>{user.firstName} {user.lastName} </span>}
+                {activeUser.id === entry.userId && container === "feed" && <span>(you) </span>}
+                <span>{convertDateTimeFromISO(entry.timeStarted).toLocaleString()}</span>
+                <p>{entry.title}</p>
+                <Button>Details</Button>
+                {history && <Button color="secondary">Edit</Button>}
+            </CardContent>
+		</Card>
 	);
 }
--- src/components/history/HistoryContainer.js
+++ src/components/history/HistoryContainer.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react';
+import { EntryContext } from '../providers/EntryProvider';
+import { UserContext } from '../providers/UserProvider';
+import HistoryList from './HistoryList';
 
-export class HistoryContainer extends Component {
-    render() {
-        return (
-            <div>
-                HISTORY CONTAINER
-            </div>
-        )
-    }
+export default function HistoryContainer(props) {
+    const { entries } = React.useContext(EntryContext)
+    const { getLoggedInUser } = React.useContext(UserContext)
+
+    const activeUser = getLoggedInUser()
+    const [userEntries, setUserEntries] = useState([])
+
+    useEffect(()=>{
+        // console.log('entries', entries);
+        const userEntries = entries.filter((entry)=>{
+            return (entry.userId === activeUser.id)
+        })
+        setUserEntries(userEntries)
+    }, [ entries ])
+
+	return (
+        <HistoryList entries={userEntries} activeUser={activeUser} />
+    )
 }
-
-export default HistoryContainer
