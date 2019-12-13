985ed7c00187498cf5e4aae572da79fe68ded83b
Ryan Bishop <rdbishop06@gmail.com>
2019-12-13 15:51:18 -0600

    filter by title and description

--- src/components/history/HistoryContainer.js
+++ src/components/history/HistoryContainer.js
@@ -2,13 +2,38 @@ import React, { useEffect, useState } from 'react';
 import { EntryContext } from '../providers/EntryProvider';
 import { UserContext } from '../providers/UserProvider';
 import HistoryList from './HistoryList';
+import { Input, Typography } from '@material-ui/core';
 
 export default function HistoryContainer(props) {
     const { entries } = React.useContext(EntryContext)
     const { getLoggedInUser } = React.useContext(UserContext)
+    const [searchTerm, setSearchTerm] = useState('')
 
     const activeUser = getLoggedInUser()
     const [userEntries, setUserEntries] = useState([])
+
+    const handleChange = e => {
+        setSearchTerm(e.target.value)
+    }
+    
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
+
+    useEffect(()=>{
+        const userEntries = entries.filter((entry)=>{
+            return (entry.userId === activeUser.id)
+        })
+        setUserEntries(userEntries)
+        const filteredEntries = userEntries.filter((entry)=>{
+            const searchLower = searchTerm.toLowerCase()
+            const title = entry.title.toLowerCase()
+            const description = entry.description.toLowerCase()
+            return title.includes(searchLower) || description.includes(searchLower)
+        })
+        // console.log('filteredEntries', filteredEntries)
+        setUserEntries(filteredEntries)
+    }, [searchTerm])
 
     useEffect(()=>{
         // console.log('entries', entries);
@@ -19,6 +44,12 @@ export default function HistoryContainer(props) {
     }, [ entries ])
 
 	return (
-        <HistoryList entries={userEntries} activeUser={activeUser} />
+        <React.Fragment>
+            <Typography>SEARCH</Typography>
+            <form onSubmit={handleSubmit}>
+                <Input placeholder="Search by keyword" value={searchTerm} onChange={handleChange}/>
+            </form>
+            <HistoryList entries={userEntries} activeUser={activeUser} />
+        </React.Fragment>
     )
 }
