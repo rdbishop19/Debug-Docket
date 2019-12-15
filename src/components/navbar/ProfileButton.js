66bd51c23864132f91a0d7d867a9673ed7f941e4
Ryan Bishop <rdbishop06@gmail.com>
2019-12-15 14:43:14 -0600

    fix: navbar home/profile buttons with dropdowns built

---
+++ src/components/navbar/ProfileButton.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { IconButton } from '@material-ui/core';
+// import { Link as RouterLink } from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
+import useBasicAuth from '../../hooks/ui/useBasicAuth';
+
+//TODO: uncomment once these components are built
+// const profile = React.forwardRef((props, ref) => <RouterLink innerRef={ref} to="/profile" {...props} />);
+// const settings = React.forwardRef((props, ref) => <RouterLink innerRef={ref} to="/settings" {...props} />);
+
+export default function HomeButton(props) {
+	const [ anchorEl, setAnchorEl ] = React.useState(null);
+    const { logout } = useBasicAuth()
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	const handleLogout = (e) => {
+		e.preventDefault();
+
+		logout();
+		props.history.push('/login');
+	};
+
+	return (
+		<div>
+			<IconButton edge="end" color="inherit" aria-label="menu" onClick={handleClick}>
+				<MenuIcon />
+			</IconButton>
+			<Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+				<MenuItem onClick={handleClose} /* component={profile} */>
+					Profile (todo)
+				</MenuItem>
+				<MenuItem onClick={handleClose} /* component={settings} */>
+					Settings (todo)
+				</MenuItem>
+				<MenuItem onClick={handleLogout}>
+					Logout
+				</MenuItem>
+			</Menu>
+		</div>
+	);
+}
