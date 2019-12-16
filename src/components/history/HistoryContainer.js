e520409083270ec2bddb0d8daf2fbebcad0c114a
Ryan Bishop <rdbishop06@gmail.com>
2019-12-15 20:56:03 -0600

    feature: filter by category/severity/priority in history view

--- src/components/history/HistoryContainer.js
+++ src/components/history/HistoryContainer.js
@@ -2,12 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { EntryContext } from '../providers/EntryProvider';
 import { UserContext } from '../providers/UserProvider';
 import HistoryList from './HistoryList';
-import { Input, Typography } from '@material-ui/core';
+import { Input, Typography, Button } from '@material-ui/core';
+import Dropdowns from '../inputform/Dropdowns'
 
 export default function HistoryContainer(props) {
     const { entries, userEntries, getUserEntries } = React.useContext(EntryContext)
     const { getLoggedInUser } = React.useContext(UserContext)
     const [searchTerm, setSearchTerm] = useState('')
+
+    const initialFilter = {
+        isCompleted: false,
+        priority: {
+            id: '',
+            label: 'n/a'
+        },
+        severity: {
+            id: '',
+            label: 'n/a'
+        },
+        category: {
+            id: '',
+            label: 'none'
+        }
+    }
+    const [filter, setFilter] = useState(initialFilter)
 
     const activeUser = getLoggedInUser()
     const [filteredEntries, setFilteredEntries] = useState([])
@@ -20,8 +38,35 @@ export default function HistoryContainer(props) {
         e.preventDefault()
     }
 
+    const handleRadioChange = e => {
+        setFilter({...filter, [e.target.name]: { id: Number(e.target.value) }})
+        // console.log('filter', filter)
+    }
+
+    const clearFilters = () =>{
+        // setFilter(initialFilter)
+        setFilteredEntries(userEntries)
+    }
+    // for when user selects radio dropdowns
     useEffect(()=>{
-        console.log('useEffect')
+        // console.log('filtering')
+        //reset list each time
+        setFilteredEntries(userEntries)
+        const filteredEntries = userEntries.filter((item)=>{
+            const { severity, priority, category } = filter
+            const { severityId, priorityId, categoryId } = item
+            const severityMatch = severity.id === severityId && severity.id !== ''
+            const priorityMatch = priority.id === priorityId && priority.id !== ''
+            const categoryMatch = category.id === categoryId && category.id !== ''
+            return severityMatch && priorityMatch && categoryMatch
+        })
+        // console.log('filteredEntries', filteredEntries)
+        setFilteredEntries(filteredEntries)
+    }, [filter])
+
+
+    useEffect(()=>{
+        // console.log('useEffect')
         setFilteredEntries(userEntries)
         const filteredEntries = userEntries.filter((entry)=>{
             const searchLower = searchTerm.toLowerCase()
@@ -37,13 +82,21 @@ export default function HistoryContainer(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [activeUser.id])
 
+    const { priority, severity, category } = filter
+    
 	return (
         <React.Fragment>
             <Typography>SEARCH</Typography>
             <form onSubmit={handleSubmit}>
                 <Input placeholder="Search by keyword" value={searchTerm} onChange={handleChange}/>
             </form>
-            <HistoryList entries={filteredEntries} activeUser={activeUser} {...props}/>
+            <br/><br/>
+            <Typography>FILTER</Typography>
+            <br/>
+            <Dropdowns priority={priority} severity={severity} category={category} handleRadioChange={handleRadioChange}/>
+            <Button variant="contained" color="secondary" onClick={clearFilters}>Clear Filters</Button>
+            <br/><br/>
+            <HistoryList entries={filteredEntries} isFiltering={userEntries > filteredEntries} activeUser={activeUser} {...props}/>
         </React.Fragment>
     )
 }
--- src/components/history/HistoryList.js
+++ src/components/history/HistoryList.js
@@ -7,7 +7,7 @@ import FeedCard from '../feed/FeedCard'
     Author: Ryan Bishop
 */
 export default function HistoryList(props) {
-    const { entries, activeUser } = props
+    const { entries, activeUser, isFiltering } = props
     return (
         <Paper>
             <Typography>
@@ -15,7 +15,13 @@ export default function HistoryList(props) {
             </Typography>
             {entries.length > 0 ? entries.map((entry)=>{
                 return <FeedCard id={entry.id} key={entry.id} entry={entry} activeUser={activeUser} container="history" {...props}/>
-            }) : <h5>No entries. Add a new bug to start your tracking journey.</h5>}
+            }) : <React.Fragment>
+                    {isFiltering ? 
+                        <h5>No entries match these search/filter criteria</h5>
+                        : <h5>No entries. Add a new bug to start your tracking journey.</h5>
+                    }
+                </React.Fragment>
+            }
         </Paper>
     )
 }
