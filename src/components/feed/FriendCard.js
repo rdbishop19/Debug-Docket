e6ef46b5a99e2382e4d9cbd56c70e26fb329f343
Ryan Bishop <rdbishop06@gmail.com>
2019-12-17 10:11:14 -0600

    refactor: moved friend logic to individual card components

---
+++ src/components/feed/FriendCard.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { Typography, Button } from '@material-ui/core'
+
+export default function FriendCard({ friend, removeFriend }) {
+    const [loadingStatus, setLoadingStatus] = useState(false)
+
+	const handleClick = (id) => {
+		console.log('clicked')
+		setLoadingStatus(loadingStatus => !loadingStatus)
+		console.log('loadingStatus', loadingStatus)
+		// removeFriend(id)
+	}
+    return (
+        <Typography >
+            <span>{friend.user.firstName} </span>
+            <span>{friend.user.lastName}</span>
+            <Button variant="contained" color="secondary" onClick={()=> handleClick(friend.id)} disabled={loadingStatus}>x</Button>
+        </Typography>
+    )
+}
