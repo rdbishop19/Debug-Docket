52c9a27578a6032a8069949cec28f1c126198219
Ryan Bishop <rdbishop06@gmail.com>
2019-12-23 16:16:28 -0600

    style: navTimer icon updated with red color

--- src/components/feed/FriendCard.js
+++ src/components/feed/FriendCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Typography, Button, Card, IconButton, CardContent, Tooltip } from '@material-ui/core';
+import { Typography, Button, Card, IconButton, CardContent, Tooltip, useTheme } from '@material-ui/core';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import PersonIcon from '@material-ui/icons/Person';
 
@@ -10,23 +10,27 @@ export default function FriendCard({ friend, removeFriend }) {
 		setLoadingStatus((loadingStatus) => !loadingStatus);
 		removeFriend(id);
 	};
+
+	const theme = useTheme();
+	const { palette: { type /* primary, secondary, error */ } } = theme;
+
 	return (
 		<Card style={{ margin: '5px' }}>
-			<CardContent style={{ padding: "0px 15px"}}>
+			<CardContent style={{ padding: '0px 15px' }}>
 				<Typography component="p">
-                    <PersonIcon style={{ marginBottom: "-5px"}} color="primary"/>
+					<Tooltip title="Unfollow friend" aria-label="remove-friend">
+						<IconButton
+							variant="contained"
+							color="default"
+							onClick={() => handleClick(friend.id)}
+							disabled={loadingStatus}
+						>
+							<HighlightOffIcon />
+						</IconButton>
+					</Tooltip>
+					<PersonIcon style={{ marginBottom: '-5px' }} color={type === 'light' ? 'primary' : 'secondary'} />
 					<span>{friend.user.firstName} </span>
 					<span>{friend.user.lastName}</span>
-					<Tooltip title="Remove friend" aria-label="remove-friend">
-					    <IconButton
-    						variant="contained"
-    						color="default"
-    						onClick={() => handleClick(friend.id)}
-    						disabled={loadingStatus}
-    					>
-    						<HighlightOffIcon />
-    					</IconButton>
-					</Tooltip>
 				</Typography>
 			</CardContent>
 		</Card>
