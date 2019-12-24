9f1f1e97871d4c22fad41c2ffee0080d5d1dde66
Ryan Bishop <rdbishop06@gmail.com>
2019-12-23 23:05:53 -0600

    comments posting to db and updating details view

--- src/components/entries/EntryDetailsCard.js
+++ src/components/entries/EntryDetailsCard.js
@@ -1,66 +1,146 @@
-import React, { useContext }/* , { useEffect } */ from 'react';
-import { Paper, Typography, Card, Button } from '@material-ui/core';
+import React, { useContext /* , { useEffect } */, useState, useRef, useEffect } from 'react';
+import { Paper, Typography, Card, Button, TextField } from '@material-ui/core';
 import { UserContext } from '../providers/UserProvider';
+import CommentRepository from '../../repositories/CommentRepository';
 
 function convertDateTimeFromISO(date) {
-    return new Date(date)
+	return new Date(date);
 }
 
-export default function EntryDetailsCard({ entry, history/* , deleteEntry */ }) {
-	const {id, userId, title, description, severity, priority, category, isCompleted, timeStarted, timeCompleted, totalWorkTime, totalBreakTime} = entry
-	
-	const { getLoggedInUser } = useContext(UserContext)
-	const isLoggedInUserEntry = getLoggedInUser().id === userId
+export default function EntryDetailsCard({ entry, history /* , deleteEntry */ }) {
+	const {
+		id,
+		userId,
+		title,
+		description,
+		severity,
+		priority,
+		category,
+		isCompleted,
+		timeStarted,
+		timeCompleted,
+		totalWorkTime,
+		totalBreakTime
+	} = entry;
+
+	const { getLoggedInUser } = useContext(UserContext);
+	const activeUserId = getLoggedInUser().id;
+	const isLoggedInUserEntry = activeUserId === userId;
 	// const displayDate = convertDateTimeFromISO(timeStarted)
 
-    // const handleDelete = () => {
-    //     deleteEntry(id).then(()=>history.push("/home"))
-    // }
+	const [ comment, setComment ] = useState('');
+	const [ commentArray, setCommentArray ] = useState([]);
+	// const comment = useRef()
+	// const handleDelete = () => {
+	//     deleteEntry(id).then(()=>history.push("/home"))
+	// }
+
+	const handleChange = (event, value) => {
+		setComment(event.target.value);
+	};
+
+	const postNewComment = () => {
+		//create comment obj w/ current timestamp
+		const newComment = {
+			text: comment,
+			entryId: entry.id,
+			userId: activeUserId,
+			timestamp: new Date().toISOString()
+		};
+		console.log(newComment);
+		CommentRepository.postNewComment(newComment)
+		.then((newComment)=>{
+			setCommentArray({
+				...commentArray,
+				newComment
+			})
+		})
+		//todo: post to DB
+
+		//todo: get new comment list then update state
+	};
+
+	const getComments = () => {
+		CommentRepository.getSingleEntryCommentList(entry.id)
+		.then(setCommentArray)
+	}
+
+	useEffect(getComments, [commentArray])
 
 	return (
-		<Paper style={{ margin: '10px 10px', textAlign: 'center' }}>
-			<Card>
-				<Typography variant="h5" component="h3">
-					BUG DETAILS
-				</Typography>
-	
-				<Typography>
-					<span>Title: </span>
-					{title}
-				</Typography>
-				<Typography>
-					<span>Description: </span>
-					{description}
-				</Typography>
-				<Typography>
-					<span>Status: </span>
-					{isCompleted ? "Closed" : "Open"}
-				</Typography>
-				<Typography>
-					<span>Submitted on: </span>
-					{new Date(timeStarted).toLocaleString()}
-				</Typography>
-				<Typography>
-					<span>Severity: </span>
-					{severity.label}
-				</Typography>
-				<Typography>
-					<span>Priority: </span>
-					{priority.label}
-				</Typography>
-				<Typography>
-					<span>Category: </span>
-					{category.label}
-				</Typography>
-				{isLoggedInUserEntry && 
-					<React.Fragment>
-						<Button variant="outlined" color="default" onClick={()=> history.push(`/home/${id}/edit`)}>Edit</Button>
-						{/* <Button variant="contained" color="secondary" onClick={deleteEntry}>Delete</Button> */}
-					</React.Fragment>
-				}
-				<Button variant="outlined" color="primary" onClick={()=> history.goBack()}>Go back</Button>
+		<React.Fragment>
+			<Paper style={{ margin: '10px 10px', textAlign: 'center' }}>
+				<Card>
+					<Typography variant="h5" component="h3">
+						BUG DETAILS
+					</Typography>
 
-			</Card>
-		</Paper>
+					<Typography>
+						<span>Title: </span>
+						{title}
+					</Typography>
+					<Typography>
+						<span>Description: </span>
+						{description}
+					</Typography>
+					<Typography>
+						<span>Status: </span>
+						{isCompleted ? 'Closed' : 'Open'}
+					</Typography>
+					<Typography>
+						<span>Submitted on: </span>
+						{new Date(timeStarted).toLocaleString()}
+					</Typography>
+					<Typography>
+						<span>Severity: </span>
+						{severity.label}
+					</Typography>
+					<Typography>
+						<span>Priority: </span>
+						{priority.label}
+					</Typography>
+					<Typography>
+						<span>Category: </span>
+						{category.label}
+					</Typography>
+					{isLoggedInUserEntry && (
+						<React.Fragment>
+							<Button variant="outlined" color="default" onClick={() => history.push(`/home/${id}/edit`)}>
+								Edit
+							</Button>
+							{/* <Button variant="contained" color="secondary" onClick={deleteEntry}>Delete</Button> */}
+						</React.Fragment>
+					)}
+					<Button variant="outlined" color="primary" onClick={() => history.goBack()}>
+						Go back
+					</Button>
+				</Card>
+			</Paper>
+			<TextField
+				id="comment"
+				type="text"
+				style={{ width: '97%', margin: '15px 15px' }}
+				placeholder="Know a possible way to solve this bug?"
+				// label="Comment"
+				// ref={comment}
+				value={comment}
+				// onKeyPress={handleKeyPress}
+				multiline
+				rows="3"
+				onChange={(event, value) => handleChange(event, value)}
+				variant="outlined"
+			/>
+			<div style={{ textAlign: 'right', marginRight: '25px' }}>
+				<Button color="primary" variant="contained" onClick={postNewComment}>
+					Comment
+				</Button>
+			</div>
+			<Typography variant="h5">Comments</Typography>
+			<Paper>
+				{commentArray.length ? commentArray.map((comment) => {
+					return <Card>{comment.text}</Card>;
+				}): <h4>No comments</h4>}
+			</Paper>
+		</React.Fragment>
 	);
 }
---
+++ src/repositories/CommentRepository.js
@@ -0,0 +1,44 @@
+import Settings from './Settings'
+
+export default {
+    // get single comment information
+    async get(id) {
+        const e = await fetch(`${Settings.remoteURL}/comments/${id}`)
+        return await e.json()
+    },
+    // create new comment
+    async postNewComment(comment) {
+        const data = await fetch(`${Settings.remoteURL}/comments`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(comment)
+        })
+        return await data.json()
+    },
+    // delete specific comment
+    async delete(id) {
+        const e = await fetch(`${Settings.remoteURL}/comments/${id}`, {
+            method: "DELETE"
+        })
+        return await e.json()
+    },
+    // get all comments from system
+    async getSingleEntryCommentList(entryId) {
+        const e = await fetch(`${Settings.remoteURL}/comments?entryId=${entryId}&_sort=timestamp`)
+        return await e.json()
+    },
+
+    // update comment account
+    async updateComment(comment){
+        const data = await fetch(`${Settings.remoteURL}/comments/${comment.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(comment)
+        })
+        return await data.json()
+    },
+}
